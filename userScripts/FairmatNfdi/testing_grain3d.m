829e74b4a23f75208d23cabfa6dec5b2e35edae9
Markus Kühbach <mkuehbach@users.noreply.github.com>
2025-04-23 14:58:41 +0200

    Exporting microstructure from Matlab/MTex grain3d class instances, next steps, export interfaces

--- userScripts/FairmatNfdi/testing_grain3d.m
+++ userScripts/FairmatNfdi/testing_grain3d.m
@@ -4,11 +4,16 @@ clear;
 fname = fullfile(mtexDataPath,'EBSD','SmallIN100_MeshStats.dream3d');
 grains = grain3d.load(fname);
 
-nexus_fpath = [pwd 'data.ebsd.SmallIN100_MeshStats.dream3d.nxs'];
+nexus_fpath = [pwd '\data.ebsd.SmallIN100_MeshStats.dream3d.nxs'];
+h5w = HdfFiveSeqHdl(nexus_fpath);
+ret = h5w.nexus_create(nexus_fpath);
+ret = h5w.nexus_open('H5F_ACC_RDWR');
+ret = h5w.nexus_close();
+
 h5w = HdfFiveSeqHdl(nexus_fpath);
 
 parent = 'microstructure';
-grpnm = [parent];
+grpnm = parent;
 attr = io_attributes();
 attr.add('NX_class', 'NXmicrostructure');
 ret = h5w.nexus_write_group(grpnm, attr);
@@ -31,10 +36,35 @@ attr = io_attributes();
 attr.add( "units", "1");
 ret = h5w.nexus_write(dsnm, double(grains.allV), attr);
 
-% find unique triangles
+%% find unique triangles that represent the interface network
 % TODO does F = grains.boundary.F yield triangles in winding order?
+grpnm = [parent '/crystals'];
+attr = io_attributes();
+attr.add('NX_class', 'NXobject');
+ret = h5w.nexus_write_group(grpnm, attr);
+grpnm = [parent '/interfaces'];
+attr = io_attributes();
+attr.add('NX_class', 'NXobject');
+ret = h5w.nexus_write_group(grpnm, attr);
+
+grpnm = [parent '/crystals'];
+dsnm = [grpnm '/number_of_crystals'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint32(length(grains), attr);
+dsnm = [grpnm '/index_offset'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint32(1), attr);
+
+n_grains = length(grains);
 uniq_facets_adjacent_grains = containers.Map;
-for i = 146:1:length(grains)
+grain_ids = zeros([1, n_grains]);
+phase_ids = zeros([1, n_grains]);
+roi_contacts = zeros([1, n_grains]);
+vols = zeros([1, n_grains]);
+oris = zeros([4, n_grains]);
+nbors = zeros([1, n_grains]);
+
+for i = 1:1:n_grains
     g = grains(i);
     grain_id = g.id;
     bnd = g.boundary;
@@ -54,6 +84,17 @@ for i = 146:1:length(grains)
     % disp(['unknown/unknown ' num2str((total_area - roi_contact_area) / total_area)]);
     % disp(['notIndexed (ROI)/unknown cnt ' num2str(roi_contact_cnt)]);
     % disp(['unknown/unknown cnt ' num2str(total_cnt - roi_contact_cnt)]);
+    
+    grain_ids(i) = g.id;
+    phase_ids(i) = g.phaseId;
+    roi_contacts(i) = ~all(no_boundary_contact);
+    vols(i) = g.volume;
+    oris(1, i) = g.meanOrientation.a;
+    oris(2, i) = g.meanOrientation.b;
+    oris(3, i) = g.meanOrientation.c;
+    oris(4, i) = g.meanOrientation.d;
+    nbors(i) = g.numNeighbors;
+        
     for j = 1:1:length(triangles_uvw)
         triplet = sort([triangles_uvw(j, :)]);
         key = [num2str(triplet(1)) '_' num2str(triplet(2)) '_' num2str(triplet(3))];
@@ -65,6 +106,36 @@ for i = 146:1:length(grains)
     end
     clearvars g grain_id bnd nbs nf triangles_uvw triangles_area triangles_phase no_boundary_contact roi_contact_area total_area roi_contact_cnt total_cnt j triplet key;
 end
+grpnm = [parent '/crystals'];
+dsnm = [grpnm '/indices_crystals'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint32(grain_ids), attr);
+dsnm = [grpnm '/indices_phase'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint32(phase_ids), attr);
+dsnm = [grpnm '/boundary_contact'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint8(roi_contact), attr);
+dsnm = [grpnm '/volume'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, volume, attr);
+dsnm = [grpnm '/number_of_neighbors'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, uint32(nbors), attr);
+grpnm = [parent '/crystals/orientation'];
+attr = io_attributes();
+attr.add('NX_class', 'NXrotations');
+ret = h5w.nexus_write_group(grpnm, attr);
+dsnm =  [grpnm '/parameterization'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, 'quaternion', attr);
+dsnm = [grpnm '/orientation'];
+attr = io_attributes();
+ret = h5w.nexus_write(dsnm, orientation, attr);
+clearvars grain_ids phase_ids roi_contacts vols oris nbors;
+
+% TODO - DONE until here
+
 boundary_facets = 0;
 nf = uint32(full(grains.numFaces));  % is sparse, use full(nf) to convert to full
 nbs = uint32(grains.numNeighbors);
